0151bde6c34a9957729c94fe85750b50de771a35
ashiekaajosephashiekaa-ui <ashiekaajosephashiekaa@gmail.com>
2026-09-07 00:51:42 +0100

    Update config.py
    config.py

--- app/config.py
+++ app/config.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 class Config:
     TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    TELEGRAM_WEBHOOK_URL = os.getenv("TELEGRAM_WEBHOOK_URL", "")
     PORT = int(os.getenv("PORT", 8000))
+    TIMEZONE = "UTC"
 
 config = Config()
